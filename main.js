c9c4d8785d6722d541d196a11e16eb9e54b42652
Ming-der Wang <mingderwang@gmail.com>
2022-04-26 15:16:14 +0800

    use Ropsten, you need to fund contract address manually

--- main.js
+++ main.js
@@ -10,74 +10,116 @@ import {
 import "dotenv/config";
 let xx = "Wait 🙉";
 
-const PRIVATE_KEY = randomPrivateKey(); //process.env.PRIVATE_KEY;
+Env.defaultName = EnvNames.TestNets;
+
+//const PRIVATE_KEY = randomPrivateKey(); //process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const KEY_NETWORK = NetworkNames.Ropsten;
 const RECEIVER = "0xE65B3A72e9d772Dd19719Dec92b1dE900fD178B0";
 async function main() {
   let batchHash = "";
   const sdk = new Sdk(PRIVATE_KEY, {
     env: EnvNames.TestNets,
-    networkName: NetworkNames.Etherspot,
+    networkName: KEY_NETWORK, // works for Ropsten too (with funds)
   });
+
+  // step 0, init sdk and account
   sdk.notifications$.subscribe((notification) =>
     console.log("notification:🌈", notification)
   );
-  await sdk.syncAccount();
 
+  // console.log("create session - token:🌦", (await sdk.createSession()).token);
+
+  await sdk.syncAccount();
   await sdk.computeContractAccount({
     sync: true,
   });
-  console.log("key account", sdk.state.account);
-
-  console.log(
-    "contract account",
-    await sdk.computeContractAccount({
-      sync: false,
-    })
-  );
-
+  console.log("key account:🎒", sdk.state.account);
+  /*
+notification:🌈 AnyNotification {
+  type: 'AccountMemberCreated',
+  recipient: '0x452f9F4d5BEaB3A9C8Fe5a515C2C1937274A0Cf9',
+  payload: {
+    account: '0x452f9F4d5BEaB3A9C8Fe5a515C2C1937274A0Cf9',
+    member: '0xFD8d1910D7D708BF465F6E365F91A2Bfdb3949AC'
+  }
+}
+*/
   await sdk.syncAccount();
   console.log("synced contract account member", sdk.state.accountMember);
-
+  /*
+synced contract account member {
+  type: 'Owner',
+  state: 'Added',
+  store: 'PersonalAccountRegistry',
+  createdAt: 2022-04-26T06:37:00.676Z,
+  updatedAt: 2022-04-26T06:37:00.676Z,
+  synchronizedAt: 2022-04-26T06:37:01.432Z
+}
+*/
   console.log(
     "get account",
     await sdk.getAccount({
       address: sdk.state.accountAddress,
     })
   );
+  /*
+  get account Account {
+  ensNode: null,
+  address: '0x452f9F4d5BEaB3A9C8Fe5a515C2C1937274A0Cf9',
+  type: 'Contract',
+  state: 'UnDeployed',
+  store: 'PersonalAccountRegistry',
+  createdAt: 2022-04-26T06:37:00.651Z,
+  updatedAt: 2022-04-26T06:37:00.651Z
+}
+*/
   console.log(
     "get account members",
     await sdk.getAccountMembers({
       account: sdk.state.accountAddress,
     })
   );
+  /*
+get account members AccountMembers {
+  items: [
+    AccountMember {
+      member: [Account],
+      type: 'Owner',
+      state: 'Added',
+      store: 'PersonalAccountRegistry',
+      createdAt: 2022-04-26T06:37:00.676Z,
+      updatedAt: 2022-04-26T06:37:00.676Z
+    }
+  ],
+  currentPage: 1,
+  nextPage: null
+}
+*/
 
-  const hash = await sdk.topUpAccount();
-  batchHash = hash;
-  console.log("transaction hash:🦊", hash);
-
-  const { state } = sdk;
   console.log(
     "p2pPaymentDepositAddress:🍎",
-    state.state$._value.p2pPaymentDepositAddress
+    sdk.state.state$._value.p2pPaymentDepositAddress
   );
-
-  sdk.notifications$.subscribe((notification) =>
-    console.log("notification:🌈", notification)
-  );
-
-  console.log("create session - token:🌦", (await sdk.createSession()).token);
-  await sdk.computeContractAccount({ sync: true });
 
   // step 0 - clear batch
   await sdk.clearGatewayBatch();
 
-  const { account } = state;
+  console.log("same contract account:🔥", sdk.state.account.address);
 
-  console.log("contract account:🔥", account.address);
-
-  // top-up contract account (account.address)
+  // top-up contract account (sdk.account.address)
+  // step 0.1 - fundings contract address otherwise, tx will be reverted
+  // only no Etherspot network
+  if (KEY_NETWORK === NetworkNames.Etherspot) {
+    console.log("balance:🐝", await sdk.getAccountBalances()); // balance will fail on Etherspot network
+    const hash = await sdk.topUpAccount(); // funding to new account
+    console.log("funding - hash:🦊", hash);
+    const hash2 = await sdk.topUpPaymentDepositAccount(); // funding to P2P Deposit account
+    console.log("funding P2P Deposit - hash:🦊", hash2);
+  }
 
   // add transaction to gateway batch
+  // step 1 - batch native token for Tx
   await sdk.batchExecuteAccountTransaction({
     to: RECEIVER,
     value: 100, // 100 wei
@@ -100,7 +142,7 @@ async function main() {
     })
     .catch((error) => {
       console.log(
-        "Transaction estimation failed with error💥💥 ",
+        "Transaction estimation failed with error💥💥 ", // may have "reverted": "Transaction reverted", without enough funds, -> to use sdk.topUpAccount()
         error.message
       );
     });
@@ -111,7 +153,6 @@ async function main() {
     while (xx !== "Sent") {
       await new Promise((r) => setTimeout(r, 2000));
       console.log(xx);
-      console.log(batchHash);
       sdk
         .getGatewaySubmittedBatch({
           hash: batchHash,
